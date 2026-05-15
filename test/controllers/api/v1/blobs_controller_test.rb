6f7fa158171e943d37cb04751212673908ec797b
BushraAljuwair <bushraaljuwair@gmail.com>
2026-05-15 18:32:55 +0300

    add integration tests

--- test/controllers/api/v1/blobs_controller_test.rb
+++ test/controllers/api/v1/blobs_controller_test.rb
@@ -1,7 +1,50 @@
 require "test_helper"
 
 class Api::V1::BlobsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  TOKEN = "Bearer my-secret-token"
+
+
+  test "should store blob" do
+    post "/api/v1/blobs",
+      params: {
+        id: "test-unit",
+        data: "SGVsbG8="
+      }.to_json,
+      headers: {
+        "Authorization" => TOKEN,
+        "Content-Type" => "application/json"
+      }
+
+    assert_response :created
+  end
+
+
+
+  test "should get blob" do
+    post "/api/v1/blobs",
+      params: {
+        id: "test-get",
+        data: "SGVsbG8="
+      }.to_json,
+      headers: {
+        "Authorization" => TOKEN,
+        "Content-Type" => "application/json"
+      }
+
+
+    get "/api/v1/blobs/test-get",
+      headers: {
+        "Authorization" => TOKEN
+      }
+
+    assert_response :success
+  end
+
+
+
+  test "should reject unauthorized request" do
+    get "/api/v1/blobs/test-unit"
+
+    assert_response :unauthorized
+  end
 end
