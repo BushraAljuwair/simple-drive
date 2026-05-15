0e8048c04145046cd5e0ebf835fa7f000caddd74
BushraAljuwair <bushraaljuwair@gmail.com>
2026-05-15 16:46:07 +0300

    add base64 validation and authentication

--- app/controllers/api/v1/blobs_controller.rb
+++ app/controllers/api/v1/blobs_controller.rb
@@ -1,81 +1,42 @@
 
-# require "base64"
-
-# class Api::V1::BlobsController < ApplicationController
-#   def create
-#     decoded_data = Base64.decode64(params[:data])
-
-#     blob = Domain::Entities::Blob.new(
-#       id: params[:id],
-#       data: decoded_data,
-#       size: decoded_data.bytesize,
-#       created_at: Time.now.utc
-#     )
-
-#     storage = Infrastructure::Storages::LocalStorage.new
-#     repository = Infrastructure::Repositories::BlobRepositoryImpl.new(storage)
-#     use_case = Domain::UseCases::StoreBlob.new(repository)
-
-#     use_case.call(blob)
-
-#     render json: { message: "blob saved" }, status: :created
-
-#   rescue => e
-#     render json: {
-#       error: e.message,
-#       class: e.class.name
-#     }, status: :bad_request
-#   end
-
-
-#   def show
-#     storage = Infrastructure::Storages::LocalStorage.new
-#     repository = Infrastructure::Repositories::BlobRepositoryImpl.new(storage)
-#     use_case = Domain::UseCases::GetBlob.new(repository)
-
-#     data = use_case.call(params[:id])
-
-#     render json: {
-#       id: params[:id],
-#       data: Base64.strict_encode64(data),
-#       size: data.bytesize,
-#       created_at: Time.now.utc
-#     }
-#   end
-# end
 require "base64"
 
 class Api::V1::BlobsController < ApplicationController
-  def create
-    decoded_data = Base64.decode64(params[:data])
-
-    blob = Domain::Entities::Blob.new(
-      id: params[:id],
-      data: decoded_data,
-      size: decoded_data.bytesize,
-      created_at: Time.now.utc
-    )
-
-    storage = Infrastructure::Storages::LocalStorage.new
-    repository = Infrastructure::Repositories::BlobRepositoryImpl.new(storage)
-    use_case = Domain::UseCases::StoreBlob.new(repository)
-
-    use_case.call(blob)
-
-    BlobRecord.create!(
-      blob_id: blob.id,
-      size: blob.size
-    )
-
-    render json: { message: "blob saved" }, status: :created
-
-  rescue => e
-    render json: {
-      error: e.message,
-      class: e.class.name
+def create
+  begin
+    decoded_data = Base64.strict_decode64(params[:data])
+  rescue ArgumentError
+    return render json: {
+      error: "Invalid Base64 data"
     }, status: :bad_request
   end
 
+  blob = Domain::Entities::Blob.new(
+    id: params[:id],
+    data: decoded_data,
+    size: decoded_data.bytesize,
+    created_at: Time.now.utc
+  )
+
+  storage = Infrastructure::Storages::LocalStorage.new
+  repository = Infrastructure::Repositories::BlobRepositoryImpl.new(storage)
+  use_case = Domain::UseCases::StoreBlob.new(repository)
+
+  use_case.call(blob)
+
+  BlobRecord.create!(
+    blob_id: blob.id,
+    size: blob.size
+  )
+
+  render json: { message: "blob saved" }, status: :created
+
+rescue => e
+  render json: {
+    error: e.message,
+    class: e.class.name
+  }, status: :bad_request
+end
 
   def show
     storage = Infrastructure::Storages::LocalStorage.new
